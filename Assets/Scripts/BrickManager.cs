ad6af16f10f046d1d9caa761effff21394172f45
vague315 <vague315@gmail.com>
2018-04-08 22:53:09 -0500

    Starts brickmanager to generate levels

---
+++ Assets/Scripts/BrickManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickManager : MonoBehaviour {
+
+	private GameObject bricksParent;
+
+	private string map = "####  ####  ####  ####";
+
+	// Use this for initialization
+	void Start () {
+		bricksParent = GameObject.Find("Bricks");
+		if(bricksParent==null){
+			bricksParent = new GameObject();
+			bricksParent.name = "Bricks";
+		}
+
+		GenerateLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void GenerateLevel(){
+		// iterate over string | # = brick
+	}
+
+}
