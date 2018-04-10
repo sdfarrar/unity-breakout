281afda67c23ffa7077dbff23ee37bdf2bf22b87
Seth Farrar <seth@ai-sys.com>
2018-04-10 13:16:57 -0500

    Prevents paddle from exiting stage

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,9 +7,13 @@ public class Paddle : MonoBehaviour {
 	public float speed = 5f;
 
 	private Bounds confines;
+	private float leftBound;
+	private float rightBound;
+	private BoxCollider2D box;
 	private Ball ball;
 
 	void Start() {
+		box = GetComponent<BoxCollider2D>();
 		ball = GetComponentInChildren<Ball>();
 		if(ball==null){
 			Vector3 ballPosition = transform.position + Vector3.up*0.335f;
@@ -17,11 +21,23 @@ public class Paddle : MonoBehaviour {
 		}
 		Stage s = stage.GetComponent<Stage>();
 		confines = s.GetBounds();
+		leftBound = s.GetLeftBound();
+		rightBound = s.GetRightBound();
 	}
 	
 	void Update() {
 		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
 		transform.position += input * Time.deltaTime * speed;
+
+		float halfWidth = box.size.x*transform.localScale.x*0.5f;
+
+
+		if((transform.position.x - halfWidth) < leftBound){
+			transform.position = new Vector3(leftBound+halfWidth, transform.position.y, transform.position.z);
+		}
+		if((transform.position.x + halfWidth) > rightBound){
+			transform.position = new Vector3(rightBound-halfWidth, transform.position.y, transform.position.z);
+		}
 
 		if(Input.GetKeyDown(KeyCode.Space)){
 			ball.Launch(GetLaunchVector());
--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Stage : MonoBehaviour {
 
+	private Transform left;
+	private BoxCollider2D leftBox;
+	private Transform right;
+	private BoxCollider2D rightBox;
+
 	private Bounds bounds;
 
 	void Start () {
+		left = transform.Find("Left");	
+		leftBox = left.GetComponent<BoxCollider2D>();
+		right = transform.Find("Right");
+		rightBox = right.GetComponent<BoxCollider2D>();
 		ComputeBounds();
 	}
 
@@ -14,7 +23,22 @@ public class Stage : MonoBehaviour {
 		return bounds;
 	}
 
+	public float GetLeftBound(){
+		return left.transform.position.x + leftBox.size.x*0.5f;
+	}
+
+	public float GetRightBound(){
+		return right.transform.position.x - rightBox.size.x*0.5f;
+	}
+
 	private void ComputeBounds(){
-		bounds = new Bounds();
+		Debug.Log("parent.position " + transform.position);
+		Debug.Log("left.position " + left.transform.position);
+		Debug.Log("right.position " + right.transform.position);
+		float leftX = left.transform.position.x;
+		float rightX = right.transform.position.x;
+		float sizeX = (Mathf.Abs(leftX) + Mathf.Abs(rightX))/2f;
+		bounds = new Bounds(transform.position, new Vector3(sizeX, 0, 0));
+		Debug.Log("bounds: " + bounds);
 	}
 }
