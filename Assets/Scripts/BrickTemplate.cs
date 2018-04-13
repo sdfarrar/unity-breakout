2db7bff944e87cc54892e8228028ae5aa830d37e
vague315 <vague315@gmail.com>
2018-04-12 22:04:24 -0500

    Creates scriptable objects for different brick types

---
+++ Assets/Scripts/BrickTemplate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Brick", menuName="Brick")]
+public class BrickTemplate : ScriptableObject {
+
+	public int health = 1;
+	public bool indestructible = false;
+	public Color color;
+
+}
